dc6860202ab5ee0fe0e680ddf24c742592d8c8cc
sivaleba <96391621+sivaleba@users.noreply.github.com>
2022-06-15 16:35:09 -0500

    Update DogClass.java

--- src/animal/DogClass.java
+++ src/animal/DogClass.java
@@ -3,6 +3,9 @@ package animal;
 public class DogClass {
     public void main(String args[]){
 	System.out.println("Dog barks wof wof....");
+	 System.out.println("cat goes meow,meow....");   
+	    
+	    
     }
     
 }
